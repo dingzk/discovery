71b940e8e1dec65e036ed218b3d5bb27a9bfd9cd
zhenkai <zhenkai@bj-m-201005a.local>
2020-06-05 12:51:26 +0800

    shared memory storage

---
+++ vintage/config.c
@@ -0,0 +1,5 @@
+//
+// Created by zhenkai on 2020/6/5.
+//
+
+#include "config.h"
---
+++ vintage/config.h
@@ -0,0 +1,15 @@
+//
+// Created by zhenkai on 2020/6/5.
+//
+
+#ifndef DISCOVERY_CONFIG_H
+#define DISCOVERY_CONFIG_H
+
+
+class Config
+{
+
+};
+
+
+#endif //DISCOVERY_CONFIG_H
